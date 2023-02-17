c1017b81fe898512b0af366c25965b16bcbb69e4
lab2_equipo16 <lab2_equipo16@LAB2_EQ16>
2023-02-17 11:43:33 -0600

    Agregar archivos de proyecto.

---
+++ POOC_intro/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace POOC_intro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wazaa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wazaa
+            // 
+            this.wazaa.BackColor = System.Drawing.Color.Yellow;
+            this.wazaa.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.wazaa.Font = new System.Drawing.Font("Snap ITC", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wazaa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.wazaa.Location = new System.Drawing.Point(109, 109);
+            this.wazaa.Name = "wazaa";
+            this.wazaa.Size = new System.Drawing.Size(187, 93);
+            this.wazaa.TabIndex = 0;
+            this.wazaa.Text = "thundercats";
+            this.wazaa.UseVisualStyleBackColor = false;
+            this.wazaa.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(383, 337);
+            this.Controls.Add(this.wazaa);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "trabajo2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button wazaa;
+    }
+}
+
---
+++ POOC_intro/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POOC_intro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+           result= MessageBox.Show("¿deseas un pantallazo azul?", "POOC_intro",MessageBoxButtons.YesNo,MessageBoxIcon.Question          );
+            if (result == DialogResult.Yes)
+            {
+
+                this.BackColor = Color.Blue;
+                this.WindowState = FormWindowState.Maximized;
+                this.FormBorderStyle = FormBorderStyle.None;
+
+                wazaa.Visible = false;
+            }
+            
+            
+            
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
